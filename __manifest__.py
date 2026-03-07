9c55b2fa82b29ba5f33496e1cbf85841da7f71ae
Dape <dabeventigan@gmail.com>
2026-03-07 18:07:37 +0000

    new: Sign up and login update

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     "author": "dape",
     "license": "LGPL-3",
     "summary": "iLead I.T Solution Custom Odoo Login Page",
-    "depends": ["web"],
+    "depends": ["web", "auth_signup"],
     "data": [
             "views/login_template.xml", 
             "views/signup_template.xml", 
--- controller/main.py
+++ controller/main.py
@@ -1,12 +1,21 @@
 from odoo import http
 from odoo.http import request
+from odoo.addons.web.controllers.home import Home
+from odoo.addons.web.controllers.utils import ensure_db
+from odoo.addons.auth_signup.controllers.main import AuthSignupHome
 
-class CustomLoginController(http.Controller):
-    @http.route('/web/login', type='http', auth="public", website=True)
-    def custom_login(self, **kwargs):
+class CustomLoginController(Home):
+
+    @http.route('/web/login', type='http', auth="public", website=True, sitemap=False)
+    def web_login(self, redirect=None, **kw):
+        ensure_db()
         if request.session.uid:
-            return request.redirect('/web')
-        return request.render('custom_login.custom_login_template')
+            return request.redirect(self._login_redirect(request.session.uid, redirect=redirect))
+        
+        return request.render('custom_login.custom_login_template', {
+            'redirect': redirect,
+            'error': kw.get('error'),
+        })
 
     @http.route('/custom/ilead_login', type='http', auth="public", methods=['POST'], csrf=False, website=True)
     def custom_do_login(self, **kwargs):
@@ -24,7 +33,6 @@ class CustomLoginController(http.Controller):
 
             uid = request.session.authenticate(request.env, credentials)
 
-            # uid = request.session.authenticate(request.db, login, password)
             if uid:
                 return request.redirect('/web')
         except Exception:
@@ -32,3 +40,33 @@ class CustomLoginController(http.Controller):
         return request.render('custom_login.custom_login_template', {
             'error': 'Wrong username or password'
         })
+
+class CustomSignupController(AuthSignupHome):
+
+    @http.route('/web/signup', type='http', auth='public', website=True, sitemap=False)
+    def web_auth_signup(self, *args, **kw):
+        qcontext = self.get_auth_signup_qcontext()
+
+        if not qcontext.get('token') and not qcontext.get('signup_enabled'):
+            return request.render('web.errors', {'message': _("Signup is disabled.")})
+
+        if request.httprequest.method == 'POST':
+            try:
+                self.do_signup(qcontext)
+                return request.redirect(qcontext.get('redirect') or '/web')
+            except Exception as e:
+                qcontext['error'] = str(e)
+
+        return request.render('custom_login.custom_signup_template', qcontext)
+
+    def _prepare_signup_values(self, qcontext):
+        values = super()._prepare_signup_values(qcontext)
+
+        values.update({
+            'email': qcontext.get('email'),
+        })
+        
+        internal_group = request.env.ref('base.group_user')
+        values['group_ids'] = [(6, 0, [internal_group.id])]
+        
+        return values
